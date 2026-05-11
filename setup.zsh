2a14a7a264270c54634b8ec5424abc677cf78474
Harald Dumdey <harald.dumdey@pixelchrome.org>
2026-05-11 21:22:47 +0200

    .osx added

--- setup.zsh
+++ setup.zsh
@@ -31,7 +31,7 @@ function install-brew() {
 
 function install-zsh-features() {
     echo -e "\nInstalling zsh features..."
-    git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions 
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
     git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ~/.oh-my-zsh/custom/plugins/zsh-autocomplete
     git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
